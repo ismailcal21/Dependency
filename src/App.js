132707ecddedac9921d6fcb205d5e62afce8099c
ismailcal21 <ismailcal21@gmail.com>
2023-07-25 06:07:07 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios"
 
 function App() {
+  const [posts, setPosts] = useState([])
+  const [changePage,setChangePage]=useState(1)
+
+  useEffect(() => {
+    axios.get(`https://jsonplaceholder.typicode.com/albums`)
+      .then(response => {
+        setPosts(response)
+        console.log(response)
+        console.log(posts.data.length)
+      })
+      
+      .catch(err => {
+      console.log("err" , err)
+    })
+  }, [changePage])
+  
+   if (posts === null) {
+     return (
+       <div>Loading...</div>
+     )
+   }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      {posts && JSON.stringify(posts)}
+      {
+        changePage <= 1 ? (
+          <>
+      <button style={{display:"none"}} onClick={()=>setChangePage(changePage=>changePage-1)}>Onceki</button>
+          
+          </>
+        ) : (
+            <>
+      <button onClick={()=>setChangePage(changePage=>changePage-1)}>Onceki</button>
+            
+            </>
+        )
+      }
+      {
+        changePage >= posts.length ? (
+          <>
+      <button style={{display:"none"}} onClick={()=>setChangePage(changePage=>changePage+1)}>Sonraki</button>
+          
+          </>
+        ) : (
+            <>
+      <button onClick={()=>setChangePage(changePage=>changePage+1)}>Sonraki</button>
+            
+            </>
+        )
+      }
+      
+      App
     </div>
   );
 }
